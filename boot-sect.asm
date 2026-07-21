530cc271dcda1a68a77e9fce9f6a683ad695f6c5
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2026-07-21 17:04:19 +0530

    accessing the secret

--- boot-sect.asm
+++ boot-sect.asm
@@ -11,7 +11,34 @@ int 0x10
 mov al, 'w'
 int 0x10
 
+mov al, "1"
+int 0x10
+mov al, the_secret
+int 0x10
+
+mov al, "2"
+int 0x10
+mov al, [the_secret]
+int 0x10
+
+mov al, "3"
+int 0x10
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+mov al, "4"
+int 0x10
+mov bx, [the_secret]
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
 jmp $
+
+the_secret:
+    db "X"
 
 times 510-($-$$) db 0
 dw 0xaa55
