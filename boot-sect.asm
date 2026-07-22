a8967aa2e0410e9ffb568e93e5ea6699f2e32ad1
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2026-07-22 10:47:04 +0530

    printing

--- boot-sect.asm
+++ boot-sect.asm
@@ -1,41 +1,49 @@
+[org 0x7c00]
 mov ah, 0x0e
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'w'
+
+mov bp, 0x8000
+mov sp, bp
+
+push "A"
+push "B"
+push "C"
+
+pop bx
+mov al, bl
 int 0x10
 
-mov al, "1"
-int 0x10
-mov al, the_secret
+pop bx
+mov al, bl
 int 0x10
 
-mov al, "2"
+pop bx
+mov al, bl
+int 0x10
+
+mov al, [0x8000]
+int 0x10
+
+mov bx, hello
+call print
+
+call print_nl
+
+mov al, " "
+int 0x10
+
+mov al, "7"
 int 0x10
 mov al, [the_secret]
 int 0x10
 
-mov al, "3"
-int 0x10
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-
-mov al, "4"
-int 0x10
-mov bx, [the_secret]
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-
 jmp $
+
+%include "print.asm"
+
+hello:
+    db "hellow world", 0
+bye_bye:
+    db "goodbye", 0 
 
 the_secret:
     db "X"
