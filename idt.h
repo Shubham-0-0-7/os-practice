5ffb99c43292a7551cf2e9e324af6052e0a3eb3a
Shubham Chhatbar <shubhamchhatbar1@gmail.com>
2026-07-25 23:52:11 +0530

    adding interrupts functionality

---
+++ idt.h
@@ -0,0 +1,24 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include <stdint.h>
+
+struct idt_entry_struct{
+    uint16_t base_low;
+    uint16_t sel;
+    uint8_t always0;
+    uint8_t flags;
+    uint16_t base_high;
+} __attribute__((packed));
+typedef struct idt_entry_struct idt_entry_t;
+
+struct idt_ptr_struct{
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+typedef struct idt_ptr_struct idt_ptr_t;
+
+void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
+void idt_init(void);
+
+#endif
